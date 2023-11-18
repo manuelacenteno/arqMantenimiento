a77385b9045dffb8fd001acfcd44df2493b2248c
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2023-11-17 23:28:40 -0300

    Agrega anotaciones para visualizarlas con swagger

--- demo/src/main/java/com/example/demo/Mantenimiento/controler/MantenimientoController.java
+++ demo/src/main/java/com/example/demo/Mantenimiento/controler/MantenimientoController.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,19 +31,21 @@ public class MantenimientoController {
     private MonopatinService monopatinService;
 
     @GetMapping
+    @Operation(summary = "Lista todos los mantenimientos realizados", description = "Busca todos los mantenimientos en el repositorio")
     public List<Mantenimiento> listarMantenimientos(){
         return repo.findAll();
     }
 
     @PostMapping
+    @Operation(summary = "Crear entrada en mantenimiento", description = "Recibe un objeto mantenimiento y lo guarda en la base de datos mongoDB")
     public void grabarMantenimiento(@RequestBody Mantenimiento mantenimiento) {
         repo.save(mantenimiento);
     }
 
 
     @GetMapping("/pedirReporteMonopatines")
+    @Operation(summary = "Reporte", description = "Solicita reporte de monopatines al microservicio de Monopatines, quien se encarga de verificar si la solicitud es de un ADMIN")
     public List<ReporteMonopatinesDTO> pedirReporte(@RequestHeader("Authorization") String authorization) {
-        System.out.println(authorization);
         List<ReporteMonopatinesDTO> reporte = monopatinService.getReporte(authorization);
         return reporte;
     }
